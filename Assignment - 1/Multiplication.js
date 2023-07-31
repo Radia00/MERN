906833b69412a3eaa7ecec04ab49e3b0df73c4c1
Radia <radia002@gmail.com>
2023-07-31 14:56:23 +0600

    Multiplication.js

--- Assignment - 1/Multiplication.js
+++ Assignment - 1/Multiplication.js
@@ -1,10 +1,5 @@
-function print_table(n, i = 1) {
-        if (i == 11) 
-            return;
-        console.log(n + " * " + i + " = " + n * i);
-        i++;  
-        print_table(n, i);
-    }
-     
-    const n = parseInt(prompt('Enter an integer: '));
-    print_table(n);
+const number = parseInt(prompt('Enter an integer: '));
+for(let i = 1; i <= 10; i++) {
+    const result = i * number;
+    console.log(`${number} * ${i} = ${result}`);
+}
