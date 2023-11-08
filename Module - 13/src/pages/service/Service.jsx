439e944831e9c3abc600d6d5a37602ddfa3fbdd5
Radia <radia002@gmail.com>
2023-11-08 14:56:38 +0600

    Add files via upload

---
+++ Module - 13/src/pages/service/Service.jsx
@@ -0,0 +1,17 @@
+function Service() {
+  return (
+    <div className="container">
+      <div className="service card card-body">
+        <h1 className="text-center">Service</h1>
+        <p className="text-center">
+          I am a Web Developer. I can provide clean code and pixel perfect
+          design. I also make website more and more interactive with web
+          animations.
+        </p>
+
+      </div>
+    </div>
+  );
+}
+
+export default Service;
