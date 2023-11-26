30c73fbf5a69aab98275163f790cb96daa95a5b1
Radia <radia002@gmail.com>
2023-11-26 23:02:20 +0600

    Add files via upload

---
+++ Module -15/Pages/CreatePage.jsx
@@ -0,0 +1,11 @@
+import CreateForm from "../Components/CreateForm";
+
+const CreatePage = () => {
+  return (
+    <div>
+      <CreateForm />
+    </div>
+  );
+};
+
+export default CreatePage;
---
+++ Module -15/Pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import ListView from "../Components/ListView";
+
+const HomePage = () => {
+  return (
+    <div>
+      <ListView />
+    </div>
+  );
+};
+
+export default HomePage;
